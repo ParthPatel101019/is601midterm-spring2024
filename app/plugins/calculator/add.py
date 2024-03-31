c7ee51709ad27b332a40f9668458ba24bef7fd12
ParthPatel101019 <157995349+ParthPatel101019@users.noreply.github.com>
2024-03-31 18:58:41 -0400

    Update add.py

--- app/plugins/calculator/add.py
+++ app/plugins/calculator/add.py
@@ -1,7 +1,7 @@
 from app.command import Command
 import logging
 
-class AddCommand(Command):
+class Add(Command):
     def execute(self, operand1, operand2):
         result = float(operand1) + float(operand2)
         logging.info(f"Adding {operand1} and {operand2}: {result}")
