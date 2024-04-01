da4b30a36b255d273522ad648226710fce63aee1
ParthPatel101019 <pp824@njit.edu>
2024-03-31 23:14:09 -0400

    dummy commit

--- app/plugins/subtract.py
+++ app/plugins/subtract.py
@@ -6,7 +6,7 @@ class Subtract(Command):
     def execute(self, operand1, operand2):
         result = float(operand1) - float(operand2)
         logging.info(f"Subtracting {operand2} from {operand1}: {result}")
-        print(f"Result: {result}")
+        print(f"Result: {result} ")
         history_manager = CalculationHistoryManager()
         history_manager.add_record('Subtract', operand1, operand2, result)
         return result
